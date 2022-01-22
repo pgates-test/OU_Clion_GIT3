210439de6b62bdeb78b698a15bcef0db82d1dfbf
pgates-test <Janick5234xz>
2022-01-22 16:22:16 +0100

    branch 1

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
+int Policz(int a, int b)
+{
+    return a+b;
+}
 int main() {
     printf("Hello, World!\n");
+    printf("w pierwszym branchu = %d",Policz(2,3));
     return 0;
 }
